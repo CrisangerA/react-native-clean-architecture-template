538b78aa5b45213b0e88a7570a2811065aed7b31
cristian alejandro <calejandro@mackbook.pro>
2024-05-16 12:57:26 -0500

    ✨ Paso 1. Archivos de configuración. Se agregan y/o modifican archivos de configuracion de proyecto, imports, linters y demas. Seccion 1 de la documentacion: Archivos de configuracion, 1.1 importacion con alias, 1.2 scripts, 1.3 linter y formateo

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  //arrowParens: 'avoid',
+  //bracketSameLine: true,
+  //bracketSpacing: false,
+  semi: true,
   singleQuote: true,
   trailingComma: 'all',
+  tabWidth: 2,
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@utils': ['./src/modules/shared/domain/utils'],
+        },
+      },
+    ],
+  ],
 };
